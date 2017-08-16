09cb2aa466c39ffb56bb85ef56315b90089ce149
Robby Bourne <robbybourne1@gmail.com>
2017-08-16 13:52:22 -0400

    Initial BackPack class added

--- Backpack.cs
+++ Backpack.cs
@@ -4,7 +4,31 @@ namespace backpack_dotnet
 {
  class Backpack
  {
-     
+     public string Type { get; set; }
+     public string Item { get; set; }
+     public bool Tracked { get; set; } = false;
+     public int TrackedItems { get; set; } = 0;
+
+     public Backpack(string type, string item)
+     {
+         Type = type;
+         Item = item; 
+     }
+
+     public int AddTrackedItem()
+     {
+         TrackedItems++;
+         return TrackedItems;
+     }
+     public int RemoveTrackedItem()
+     {
+         if (TrackedItems > 0)
+         {
+             TrackedItems--;
+         }
+         return TrackedItems;
+     }
+    public
  }
 
 }
